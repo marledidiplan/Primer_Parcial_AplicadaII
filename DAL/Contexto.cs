d466326a2b202cd14aac72607bc6bdd2d2e73da0
Marledi Alcenia Diplan Reyes <marledi_diplan@ucne.edu.do>
2019-02-02 00:13:27 -0400

    BLL And Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public DbSet<CuentasBancarias> cuentasBancarias { get; set; }
+        public DbSet<Depositos> depositos { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+
     }
 }
