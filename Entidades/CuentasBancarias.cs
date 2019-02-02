991389a192503dd407d90ee3716b5c104d3d4ef2
Marledi Alcenia Diplan Reyes <marledi_diplan@ucne.edu.do>
2019-02-02 00:00:49 -0400

    Entidades...

---
+++ Entidades/CuentasBancarias.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class CuentasBancarias
+    {
+        [Key]
+        public int CuentaId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Nombre { get; set; }
+        public decimal Balance { get; set; }
+
+        public CuentasBancarias()
+        {
+            CuentaId = 0;
+            Fecha = DateTime.Now;
+            Nombre = string.Empty;
+            Balance = 0;
+        }
+        public CuentasBancarias(int cuentaId, DateTime fecha, string nombre, decimal balance)
+        {
+            CuentaId = cuentaId;
+            Fecha = fecha;
+            Nombre = nombre;
+            Balance = balance;
+        }
+    }
+}
---
+++ Entidades/Depositos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Depositos
+    {
+        [Key]
+        public int DepositoId { get; set; }
+        public int CuentaId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Concepto { get; set; }
+        public decimal Monto { get; set; }
+
+        public Depositos()
+        {
+            DepositoId = 0;
+            CuentaId = 0;
+            Fecha = DateTime.Now;
+            Concepto = string.Empty;
+            Monto = 0;
+        }
+        public Depositos(int depositoId, int cuentaId, DateTime fecha, string concepto, decimal monto)
+        {
+            DepositoId = depositoId;
+            CuentaId = cuentaId;
+            Fecha = fecha;
+            Concepto = concepto;
+            Monto = monto;
+
+        }
+    }
+}
